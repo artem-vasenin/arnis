1eedc7f864e104baa792f7a63a8ba130b306a14e
Rusich <artem.skobar@yandex.ru>
2019-09-01 00:08:02 +0300

    rusich: start layout.

--- site/templates/src/js/index.js
+++ site/templates/src/js/index.js
@@ -10,94 +10,84 @@ import VueTheMask from 'vue-the-mask';
 // import Test from './modules/test';
 import 'lightbox2';
 
-const { yandexMap, ymapMarker } = require('vue-yandex-maps');
 Vue.use(VueTheMask);
 
 new Vue({
-  components: { yandexMap, ymapMarker, axios },
+  components: { axios },
   el: '#site',
   data() {
     return {
-      mapCenter: window.map_center,
-      mapPoint: window.map_point,
-      mapZoom: window.map_zoom,
-      mapDesc: window.map_desc,
-      mapDrag: window.map_drag ? 'drag' : '',
-      mapScroll: window.map_scroll ? 'scrollZoom' : '',
-      mapIcon: {color: 'red', glyph: 'home'},
-      form: {},
     };
   },
   methods: {
-    getFields() {
-      let fields = [];
-      for(let key in this.form) {
-        if (key.indexOf("field_") !== -1) {
-          fields.push(this.form[key]);
-        }
-      }
-      return fields;
-    },
-    formValidation() {
-      let error = [];
-      const fields = this.getFields();
-      for(let i = 0; i < fields.length; i++) {
-        if (!fields[i].fieldValue) {
-          error.push(fields[i]);
-        }
-      }
-      return !error.length;
-    },
-    clearForm(form) {
-      for (var key in form) {
-        if (key.indexOf("field_") !== -1 ) {
-          form[key].fieldValue = '';
-        }
-      }
-    },
-    sendForm() {
-      const app = this;
-      const fields = this.getFields().map(item => {
-          return {
-            fieldError: item.fieldError,
-            fieldID: item.fieldID,
-            fieldTitle: item.fieldTitle,
-            fieldName: item.fieldName,
-            fieldRequired: item.fieldRequired,
-            fieldType: item.fieldType,
-            fieldValue: item.fieldValue,
-          };
-        }
-      );
-      const form = {
-        formId: app.form.id,
-        formEmail: app.form.email,
-        formTheme: app.form.theme,
-        fields: fields,
-        formSuccess: app.formSuccess,
-        formError: app.formError,
-      };
-
-      $.post("/settings/mail/", form)
-        .done((data) => {
-          // this.clearForm(this.form);
-          // this.form.modal.class = 'modal-info';
-          this.form.modal.message = this.form.success;
-          $('#formModal').modal('show');
-        })
-        .fail((error) => {
-          console.log('status:', error.status, ', text: ', error.statusText);
-          // this.form.modal.class = 'modal-danger';
-          this.form.modal.message = this.form.error;
-          $('#formModal').modal('show');
-        })
-        .always(() => {
-          // console.log('allways');
-        });
-    },
+    // getFields() {
+    //   let fields = [];
+    //   for(let key in this.form) {
+    //     if (key.indexOf("field_") !== -1) {
+    //       fields.push(this.form[key]);
+    //     }
+    //   }
+    //   return fields;
+    // },
+    // formValidation() {
+    //   let error = [];
+    //   const fields = this.getFields();
+    //   for(let i = 0; i < fields.length; i++) {
+    //     if (!fields[i].fieldValue) {
+    //       error.push(fields[i]);
+    //     }
+    //   }
+    //   return !error.length;
+    // },
+    // clearForm(form) {
+    //   for (var key in form) {
+    //     if (key.indexOf("field_") !== -1 ) {
+    //       form[key].fieldValue = '';
+    //     }
+    //   }
+    // },
+    // sendForm() {
+    //   const app = this;
+    //   const fields = this.getFields().map(item => {
+    //       return {
+    //         fieldError: item.fieldError,
+    //         fieldID: item.fieldID,
+    //         fieldTitle: item.fieldTitle,
+    //         fieldName: item.fieldName,
+    //         fieldRequired: item.fieldRequired,
+    //         fieldType: item.fieldType,
+    //         fieldValue: item.fieldValue,
+    //       };
+    //     }
+    //   );
+    //   const form = {
+    //     formId: app.form.id,
+    //     formEmail: app.form.email,
+    //     formTheme: app.form.theme,
+    //     fields: fields,
+    //     formSuccess: app.formSuccess,
+    //     formError: app.formError,
+    //   };
+    //
+    //   $.post("/settings/mail/", form)
+    //     .done((data) => {
+    //       // this.clearForm(this.form);
+    //       // this.form.modal.class = 'modal-info';
+    //       this.form.modal.message = this.form.success;
+    //       $('#formModal').modal('show');
+    //     })
+    //     .fail((error) => {
+    //       console.log('status:', error.status, ', text: ', error.statusText);
+    //       // this.form.modal.class = 'modal-danger';
+    //       this.form.modal.message = this.form.error;
+    //       $('#formModal').modal('show');
+    //     })
+    //     .always(() => {
+    //       // console.log('allways');
+    //     });
+    // },
   },
   mounted() {
-    this.form = window.form || {};
   }
 });
 
